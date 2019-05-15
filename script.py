b8dbfd06187352932b1f99a7c395f689e7450c70
Maryann <mfoley@stuy.edu>
2019-05-15 15:22:52 -0400

    done!

--- script.py
+++ script.py
@@ -41,7 +41,7 @@ def run(filename):
     coords = []
     coords1 = []
     symbols['.white'] = ['constants',
-                         {'red': [0.6, 0.7, 0.8],
+                         {'red': [0.2, 0.5, 0.5],
                           'green': [0.2, 0.5, 0.5],
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
@@ -58,51 +58,66 @@ def run(filename):
 
         elif command["op"] == 'sphere':
             #print 'SPHERE\t' + str(args)
+            if "constants" in command and command["constants"] is not None:
+                reflect=command["constants"]
+            else:
+                reflect=".white"
             add_sphere(coords,
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step_3d)
             matrix_mult(stack[-1], coords)
             draw_polygons(coords, screen, zbuffer, view, ambient, light,symbols,reflect)
-            polygons = []
+            coords = []
 
         elif command["op"] == 'torus':
             #print 'TORUS\t' + str(args)
+            if "constants" in command and command["constants"] is not None:
+                reflect=command["constants"]
+            else:
+                reflect=".white"
+            print(coords)
             add_torus(coords,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), step_3d)
             matrix_mult(stack[-1], coords)
             draw_polygons(coords, screen, zbuffer, view, ambient, light, symbols,reflect)
-            polygons = []
+            coords = []
 
         elif command["op"] == 'box':
             #print 'BOX\t' + str(args)
+            if "constants" in command and command["constants"] is not None:
+                reflect=command["constants"]
+            else:
+                reflect=".white"
             add_box(coords,
                     float(args[0]), float(args[1]), float(args[2]),
                     float(args[3]), float(args[4]), float(args[5]))
             matrix_mult(stack[-1], coords)
             draw_polygons(coords, screen, zbuffer, view, ambient, light, symbols,reflect)
-            polygons = []
+            coords = []
 
-        elif command["op"] == 'circle':
-            #print 'CIRCLE\t' + str(args)
-            add_circle(edges,
-                       float(args[0]), float(args[1]), float(args[2]),
-                       float(args[3]), step)
-            matrix_mult(stack[-1], edges)
-            draw_lines(edges, screen, zbuffer, color)
-            edges = []
+            '''
+            elif command["op"] == 'circle':
+                #print 'CIRCLE\t' + str(args)
+                add_circle(edges,
+                           float(args[0]), float(args[1]), float(args[2]),
+                           float(args[3]), step)
+                matrix_mult(stack[-1], edges)
+                draw_lines(edges, screen, zbuffer, color)
+                edges = []
 
-        elif command["op"] == 'hermite' or command["op"] == 'bezier':
-            #print 'curve\t' + command["op"] + ": " + str(args)
-            add_curve(edges,
-                      float(args[0]), float(args[1]),
-                      float(args[2]), float(args[3]),
-                      float(args[4]), float(args[5]),
-                      float(args[6]), float(args[7]),
-                      step, command["op"])
-            matrix_mult(stack[-1], edges)
-            draw_lines(edges, screen, zbuffer, color)
-            edges = []
+            elif command["op"] == 'hermite' or command["op"] == 'bezier':
+                #print 'curve\t' + command["op"] + ": " + str(args)
+                add_curve(edges,
+                          float(args[0]), float(args[1]),
+                          float(args[2]), float(args[3]),
+                          float(args[4]), float(args[5]),
+                          float(args[6]), float(args[7]),
+                          step, command["op"])
+                matrix_mult(stack[-1], edges)
+                draw_lines(edges, screen, zbuffer, color)
+                edges = []
+            '''
 
         elif command["op"] == 'line':
             #print 'LINE\t' + str(args)
@@ -140,17 +155,18 @@ def run(filename):
             matrix_mult(stack[-1], t)
             stack[-1] = [x[:] for x in t]
 
-        elif command["op"] == 'ident':
-            ident(transform)
+            '''
+            elif command["op"] == 'ident':
+                ident(transform)
 
-        elif command["op"] == 'apply':
-            matrix_mult( transform, edges )
-            matrix_mult( transform, polygons )
+            elif command["op"] == 'apply':
+                matrix_mult( transform, edges )
+                matrix_mult( transform, polygons )
 
-        elif command["op"] == 'clear':
-            clear_screen(screen)
-            clear_zbuffer(zbuffer)
-
+            elif command["op"] == 'clear':
+                clear_screen(screen)
+                clear_zbuffer(zbuffer)
+            '''
         elif command["op"] == 'display' or command["op"] == 'save':
             #clear_screen(screen)
             if command["op"] == 'display':
